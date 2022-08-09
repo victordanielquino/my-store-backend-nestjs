eba7f2becfe3411e4fd77f82f8f30b3591d9ce17
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-08-09 14:42:49 -0400

    migra

--- src/modules/database/data.sources.ts
+++ src/modules/database/data.sources.ts
@@ -2,15 +2,15 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const Config: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  /*host: 'localhost',
   port: 5432,
   username: 'store',
   password: 'store',
-  database: 'dbstore',
-  /*url: process.env.DATABASE_URL,
+  database: 'dbstore',*/
+  url: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true,
-  },*/
+  },
   entities: ['src/core/models/entities/*.entity.ts'],
   migrations: ['src/modules/database/migrations/*.ts'],
   migrationsTableName: 'custom_migration_table',
